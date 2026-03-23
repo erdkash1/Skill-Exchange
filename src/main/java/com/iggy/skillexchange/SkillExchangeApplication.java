2d7d37fe3b6edb1cdd440fadbde696d2e9f27156
Erdkash1 <erdkash1@gmail.com>
2026-03-22 21:44:28 -0500

    initial project setup - Skill Exchange API

---
+++ src/main/java/com/iggy/skillexchange/SkillExchangeApplication.java
@@ -0,0 +1,13 @@
+package com.iggy.skillexchange;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkillExchangeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SkillExchangeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/iggy/skillexchange/SkillExchangeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iggy.skillexchange;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkillExchangeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
